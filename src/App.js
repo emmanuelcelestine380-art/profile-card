24e80245ace75bd569eeec6e4bc126cc6fd8926c
emmanuelcelestine380-art <emmanuelcelestine380@gmail.com>
2025-09-03 08:16:48 +0100

    Create src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ProfileCard from "./ProfileCard";
+
+function App() {
+  return (
+    <div className="app">
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
